5201b775c060f0a84036c30184fafc868d082d6f
sestudiantesena-debug <sestudiantesena@gmail.com>
2026-04-21 04:28:38 +0000

    PRUEBA2

--- src/App.tsx
+++ src/App.tsx
@@ -633,7 +633,7 @@ export function App() {
           </div>
         </div>
         <div className="footer-bottom">
-          <div className="footer-copy">© 2025 GuajiraTech · Todos los derechos reservados Jarlin Esquivel</div>
+          <div className="footer-copy">© 2025 GuajiraTech Pro · Todos los derechos reservados Jarlin Esquivel</div>
           <div className="footer-socials">
             <div className="social-btn">
               <svg viewBox="0 0 24 24"><path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" /></svg>
